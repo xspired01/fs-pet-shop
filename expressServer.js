160dd7e766ab82de963772533cab3d20882b293f
xspired01 <don.spire1@gmail.com>
2017-02-22 15:09:28 -0800

    cleaned up

--- expressServer.js
+++ expressServer.js
@@ -50,39 +50,3 @@ app.listen(port, function(){
 });
 
 module.exports = app;
-
-// 'use strict'
-//
-// var fs = require('fs');
-// var path = require('path');
-// var petsPath = path.join(__dirname, 'pets.json');
-// var express = require('express');
-// var app = express();
-// var port = 8000;
-//
-// app.disable('x-powered-by');
-//
-// app.get('/pets/:id', function(req, res){
-//   fs.readFile(petPath, 'utf8', function(err, petsJSON){
-//     if (err) {
-//       console.error(err.stack);
-//       return res.sendStatus(500);
-//     }
-//     var id = Number.parseInt(req.params.id);
-//     var pets = JSON.parse(petsJSON);
-//
-//     if (id < 0 || id >= pets.length || Number.isNaN(id)){
-//       res.set('Content-Type', 'text/plain');
-//       return res.sendStatus(404);
-//     }
-//
-//     res.send(pets[id]);
-//   });
-// });
-// app.use(function(req, res){
-//   res.sendStatus(404);
-// });
-//
-// app.listen(port, function(){
-//   console.log('Listening on port ' + port);
-// });
